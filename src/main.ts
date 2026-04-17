4cb1f23cda2f674fe057d4dc4e81313f7f1b4f49
BrendonLee23 <brendow_prime@hotmail.com>
2026-04-17 12:29:35 -0300

    fix: use dynamic PORT env variable for production

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  await app.listen(3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
